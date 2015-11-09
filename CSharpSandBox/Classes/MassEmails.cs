0fdd5a13a759a09c77096c25812cb603918bf428
smalltom70 <smalltom70@hotmail.com>
2015-11-09 21:09:50 +0000

    Initial commit

---
+++ CSharpSandBox/Classes/MassEmails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSandBox.Classes
+{
+    class EmailData
+    {
+        public class MassEmail
+        {
+            public string Id { get; set; }
+            public string Subject { get; set; }
+            public string SecurityLevel { get; set; }
+            
+        }
+
+        public class MassEmails
+        {
+            public List<MassEmail> MassEmail { get; set; }
+        }
+
+        public class RootObject
+        {
+            public MassEmails MassEmails { get; set; }
+        }
+    }
+}
---
+++ CSharpSandBox/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpSandBox.Classes;
+using Newtonsoft.Json;
+
+namespace CSharpSandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new WebClient();
+
+            var emailData = c.DownloadString("..\\..\\Data\\MassEmail.json");
+            var d = new Dictionary<string,EmailData.RootObject>();
+
+            d.Add("MassEmailCatalogue", JsonConvert.DeserializeObject<EmailData.RootObject>(emailData));
+           
+          
+
+
+
+        }
+    }
+}
